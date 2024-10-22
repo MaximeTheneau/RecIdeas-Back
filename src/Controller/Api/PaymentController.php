c133c29decae43af0acfa1c13131c73ed6d47f47
Theneau Maxime <theneau.maxime@gmail.com>
2024-10-22 20:32:48 +0200

    Feat: Make entity donors

--- src/Controller/Api/PaymentController.php
+++ src/Controller/Api/PaymentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Donor;
+use Doctrine\ORM\EntityManagerInterface;
 use Stripe\Stripe;
 use Stripe\Checkout\Session;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -15,6 +17,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 )]
 class PaymentController extends ApiController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/create-checkout-session', name: 'create_checkout_session', methods: ['POST'])]
     public function createCheckoutSession(Request $request): JsonResponse
     {
@@ -23,9 +32,13 @@ class PaymentController extends ApiController
         $content = json_decode($request->getContent(), true);
         $amount = $content['amount'];
         $name = $content['name'];
-        $messge = $content['message'];
-
-
+        $message = $content['message'];
+        $locale = $content['locale'];
+        
+        if (strlen($name) > 70 || strlen($message) > 70) {
+            return new JsonResponse(['error' => 'Error 70 caractères.'], 400);
+        }
+        
         try {
             $session = Session::create([
                 'payment_method_types' => ['card'],
@@ -40,13 +53,40 @@ class PaymentController extends ApiController
                     'quantity' => 1,
                 ]],
                 'mode' => 'payment',
-                'success_url' => $_ENV['DOMAIN_FRONT'] . '/donate?status=success',
-                'cancel_url' => $_ENV['DOMAIN_FRONT'] . '/donate?status=error',
+                'success_url' => $_ENV['DOMAIN_FRONT'] . $locale . '/dons?status=success',
+                'cancel_url' => $_ENV['DOMAIN_FRONT'] .  $locale . '/dons?status=error',
             ]);
+
+            $donor = new Donor();
+            $donor->setName(!empty($name) ? $name : 'Anonymous');
+            $donor->setMessage($message);
+            $donor->setAmount($amount);
+            $donor->setLocale($locale);
+
+            $this->entityManager->persist($donor);
+            $this->entityManager->flush();
 
             return new JsonResponse(['id' => $session->id]);
         } catch (\Exception $e) {
             return new JsonResponse(['error' => $e->getMessage()], 500);
         }
+    }
+
+    #[Route('/donors', name: 'get_donors', methods: ['GET'])]
+    public function getDonors(): JsonResponse
+    {
+        $donors = $this->entityManager->getRepository(Donor::class)->findBy([], ['id' => 'DESC']);
+        
+        $donorData = array_map(function($donor) {
+            return [
+                'id' => $donor->getId(),
+                'name' => $donor->getName(),
+                'message' => $donor->getMessage(),
+                'amount' => $donor->getAmount(),
+                'locale' => $donor->getLocale(),
+            ];
+        }, $donors);
+
+        return new JsonResponse($donorData);
     }
 }
---
+++ src/Entity/Donor.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DonorRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DonorRepository::class)]
+class Donor
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 70)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 70)]
+    private ?string $message = null;
+
+    #[ORM\Column]
+    private ?int $amount = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $locale = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getLocale(): ?string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): static
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+}
